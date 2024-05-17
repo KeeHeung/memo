edb892a0bd0e5550d1c90e2b2301b78a1a861cc4
keeheung <xl7923@naver.com>
2024-05-17 19:19:35 +0900

    SpringBoot의 JPA

--- src/main/java/com/sparta/memo/entity/Memo.java
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -1,17 +1,24 @@
 package com.sparta.memo.entity;
 
 import com.sparta.memo.dto.MemoRequestDto;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity // JPA가 관리할 수 있는 Entity 클래스 지정
 @Getter
 @Setter
+@Table(name = "memo") // 매핑할 테이블의 이름을 지정
 @NoArgsConstructor
 public class Memo {
-    private Long id; // 메모끼리 구분하기 윈한 필드
-    private String username; // 메모 작성자의 이름
-    private String contents; // 메모의 내용
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "username", nullable = false)
+    private String username;
+    @Column(name = "contents", nullable = false, length = 500)
+    private String contents;
 
     public Memo(MemoRequestDto requestDto) {
         this.username = requestDto.getUsername();
